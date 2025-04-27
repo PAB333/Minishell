e5b0e05a6a4663bac643ff4c7e5de08f2d2db710
PAB333 <pabrd3@gmail.com>
2025-04-27 15:55:21 +0200

    .h update

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 16:58:33 by pibreiss          #+#    #+#             */
-/*   Updated: 2025/04/21 17:50:23 by pibreiss         ###   ########.fr       */
+/*   Updated: 2025/04/27 15:53:21 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define MINISHELL_H
 
 #include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_putstr(char *str);
 
 #endif
