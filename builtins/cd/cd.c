ad4f00dfbd1e95afb2886f10d1d2101f4d9fc2c4
PAB333 <pabrd3@gmail.com>
2025-06-22 19:24:00 +0200

    cd : update env

--- builtins/cd/cd.c
+++ builtins/cd/cd.c
@@ -6,37 +6,58 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 06:56:42 by pibreiss          #+#    #+#             */
-/*   Updated: 2025/06/18 17:57:11 by pibreiss         ###   ########.fr       */
+/*   Updated: 2025/06/22 19:22:36 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char	*ft_cd(t_cmd *cmd)
+void	update_envp(t_env **env, char *old_cwd)
 {
-	int		result_path;
 	char	*cwd;
+	t_env	*tmp;
 
-	result_path = chdir(cmd->args[1]);
-	if (result_path == -1)
+	tmp = *env;
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
 	{
 		perror("cd");
-		cwd = getcwd(NULL, 0);
-		if (!cwd)
-		{
-			perror("cd");
-			free(cwd);
-		}
-		return (cwd);
+		free(old_cwd);
 	}
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->name, "PWD="))
+		{
+			free(tmp->value);
+			tmp->value = ft_strdup(cwd);
+		}
+		else if (ft_strcmp(tmp->name, "OLDPWD="))
+		{
+			free(tmp->value);
+			tmp->value = ft_strdup(old_cwd);
+		}
+		tmp = tmp->next;
+	}
+	free(cwd);
+}
+
+void	ft_cd(t_cmd *cmd, t_env **env)
+{
+	int		result_path;
+	char	*old_cwd;
+
+	old_cwd = getcwd(NULL, 0);
+	if (!old_cwd)
+	{
+		perror("cd");
+		free(old_cwd);
+	}
+	result_path = chdir(cmd->args[1]);
+	if (result_path == -1)
+		perror("cd");
 	else
 	{
-		cwd = getcwd(NULL, 0);
-		if (!cwd)
-		{
-			perror("cd");
-			free(cwd);
-		}
-		return (cwd);
+		update_envp(env, old_cwd);
+		free(old_cwd);
 	}
 }
